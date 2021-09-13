9291a3ba634f801522d1ec952be0964f4eeae8f8
AwesomeCore1 <51826058+AwesomeCore1@users.noreply.github.com>
2021-09-13 20:28:07 +0200

    I was retarded

--- craco.config.js
+++ craco.config.js
@@ -1,26 +1,27 @@
-const zlib = require("zlib");
+const zlib = require('zlib');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
 	style: {
 		postcss: {
-			plugins: [require('tailwindcss'), require('autoprefixer')],
-		},
+			plugins: [require('tailwindcss'), require('autoprefixer')]
+		}
 	},
 	webpack: {
 		plugins: [
 			new CompressionPlugin({
-				filename: "[path][base].br",
-				algorithm: "brotliCompress",
+				filename: '[path][base].br',
+				algorithm: 'brotliCompress',
 				test: /\.(js|css|html|svg)$/,
 				compressionOptions: {
 					params: {
-					[zlib.constants.BROTLI_PARAM_QUALITY]: 11,
-					},
+						[zlib.constants.BROTLI_PARAM_QUALITY]: 11
+					}
 				},
 				threshold: 10240,
 				minRatio: 0.8,
-				deleteOriginalAssets: false,
-			}),
-		],
-	},
+				deleteOriginalAssets: false
+			})
+		]
+	}
 };
