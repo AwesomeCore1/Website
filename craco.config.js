5351ee3c012813efc4d125d371c3301ffecc4a4f
AwesomeCore1 <51826058+AwesomeCore1@users.noreply.github.com>
2021-09-13 20:17:51 +0200

    Added brotli support

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,26 @@
+const zlib = require("zlib");
+
 module.exports = {
 	style: {
 		postcss: {
 			plugins: [require('tailwindcss'), require('autoprefixer')],
 		},
 	},
+	webpack: {
+		plugins: [
+			new CompressionPlugin({
+				filename: "[path][base].br",
+				algorithm: "brotliCompress",
+				test: /\.(js|css|html|svg)$/,
+				compressionOptions: {
+					params: {
+					[zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+					},
+				},
+				threshold: 10240,
+				minRatio: 0.8,
+				deleteOriginalAssets: false,
+			}),
+		],
+	}
 };
